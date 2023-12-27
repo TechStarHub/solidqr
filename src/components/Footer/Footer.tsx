53dc98d78097a2e9a797745a92beac286191913c
Bijoy Kar <bijoypoco12345@gmail.com>
2023-12-27 15:27:35 +0530

    [update]: Footer copyright, description, links added
    Footer Component Changes
    - App description added
    - TechStarHub Links added
    - App copyright added

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -3,10 +3,34 @@ import { A } from "@solidjs/router";
 
 export default function Footer() {
   return (
-    <div class="w-full p-4 flex items-center">
-      <A href="/" class="">
-        <Logo />
-      </A>
-    </div>
+    <footer class="w-full flex flex-col gap-2 p-4">
+      <div class="w-full flex flex-col sm:flex-row gap-4 justify-between items-start sm:items-center">
+        <div class="">
+          <A href="/" class="">
+            <Logo />
+          </A>
+          <p class="mt-2 text-slate-600 text-md hover:text-slate-800 transition-all duration-200">
+            SolidQR is QR generator made with SolidJS.
+          </p>
+        </div>
+        <div class="w-full sm:w-fit">
+          <p class="text-slate-600 text-md text-center font-medium ">
+            Made with ❤️ by{" "}
+            <A
+              href="https://github.com/TechStarHub"
+              target="_blank"
+              rel="noopener noreferrer"
+              class="text-[#2c4e82]"
+            >
+              @TechStarHub
+            </A>
+          </p>
+        </div>
+      </div>
+      <hr />
+      <p class="text-center text-sm font-medium text-slate-500 hover:text-slate-700 transition-all duration-200">
+        © 2023 BioDrop. All rights reserved.
+      </p>
+    </footer>
   );
 }
